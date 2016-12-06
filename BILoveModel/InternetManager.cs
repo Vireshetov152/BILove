b909e527e90f5b8336136a3998eb066eb7dd5908
Vireshetov152 <Vireshetov152@gmail.com>
2016-12-07 01:02:28 +0300

    VKApi

--- BILoveModel/InternetManager.cs
+++ BILoveModel/InternetManager.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VkNet;
+using VkNet.Enums.Filters;
 
 namespace BILoveModel
 {
-    class InternetManager
+    public class InternetManager
     {
+        public Dictionary<string, string> VKAuthorization(string login, string password)
+        {
+            ulong appID = 5764856;                      
+            string email = login;         
+            string pass = password;               
+            Settings scope = Settings.All;    
+
+            var vk = new VkApi();
+            vk.Authorize(new ApiAuthParams
+            {
+                ApplicationId = appID,
+                Login = email,
+                Password = pass,
+                Settings = scope
+            });
+
+            var infoDict = new Dictionary<string, string>();
+
+            var user = vk.Users.Get(vk.UserId.Value, ProfileFields.All);
+            var userName = user.FirstName + " " + user.LastName;
+            var userPhotoUrl = user.Photo100.OriginalString;
+
+            infoDict.Add("userName", userName);
+            infoDict.Add("userPhoto", userPhotoUrl);
+
+            Console.WriteLine(infoDict["userPhoto"]);
+            
+            return infoDict;
+        }   
     }
 }
--- BILoveModel/Program.cs
+++ BILoveModel/Program.cs
@@ -10,6 +10,8 @@ namespace BILoveModel
     {
         static void Main(string[] args)
         {
+            InternetManager m = new InternetManager();
+            m.VKAuthorization("", "");
         }
     }
 }
